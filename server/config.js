f8cefd1cf77ca34e216f60b0edbfdd9d087867fb
Dhanekula Yaswanth <111735437+DhanekulaYaswanth@users.noreply.github.com>
2023-10-02 12:22:55 +0530

    added changes for server

--- server/config.js
+++ server/config.js
@@ -2,6 +2,8 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log(process.env.email,process.env.password)
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
